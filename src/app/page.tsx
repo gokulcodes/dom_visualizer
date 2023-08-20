7c20b30a0b41a38e13dc4e05a560f3eb1edee0f2
gokulcodes <gokul.varadan@outlook.com>
2023-08-20 09:10:43 +0530

    initial basic editor

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useRef } from "react"
+
+export default function Home() {
+  const codeRef = useRef<HTMLInputElement>(null)
+  const domRef = useRef<HTMLInputElement>(null)
+
+  function onCodeChange() {
+    if (domRef.current && codeRef.current) {
+      domRef.current.insertAdjacentHTML("afterbegin", codeRef.current.value)
+    }
+  }
+
+  return (
+    <div className="root">
+      <main className="sub-root">
+
+        <section id="coding-area" className="sub-root_section" >
+          <textarea autoFocus ref={codeRef} className="code_textarea" >
+          </textarea>
+          <button onClick={onCodeChange} className="code_run_button" >Run</button>
+        </section>
+
+        <section ref={domRef} id="coding-area" className="sub-root_section" >
+          <div className="dom-visualizer ">
+
+          </div>
+        </section>
+
+      </main>
+    </div>
+  )
+}
